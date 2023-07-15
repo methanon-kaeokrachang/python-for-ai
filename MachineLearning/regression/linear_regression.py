c83583d01dd3d482111379c47fc77e6a5697a530
methanon-kaeokrachang <138145141+methanon-kaeokrachang@users.noreply.github.com>
2023-07-15 22:26:44 +0700

    Update linear_regression.py

--- MachineLearning/regression/linear_regression.py
+++ MachineLearning/regression/linear_regression.py
@@ -1,1 +1,15 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Sample data for days since the outbreak and corresponding number of COVID-19 cases
+days = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+cases = np.array([10, 15, 20, 30, 45, 60, 75, 90, 105, 120])
+
+# Plotting the dataset
+plt.scatter(days, cases, color='blue', label='Actual Cases')
+plt.xlabel('Days since outbreak')
+plt.ylabel('Number of COVID-19 cases')
+plt.title('COVID-19 Dataset')
+plt.legend()
+plt.show()
