26829e14a37123dc58a26636bbe6610d1ba45fc1
methanon-kaeokrachang <138145141+methanon-kaeokrachang@users.noreply.github.com>
2023-07-22 21:18:49 +0700

    Create face_recognition.py

---
+++ MachineLearning/application/face_recognition.py
@@ -0,0 +1,37 @@
+#Donwload 'OpenCV'
+!pip install opencv-python
+
+#Import OpenCV to our project
+import cv2
+
+#assign image into variable
+imagePath = 'pita.jpg'
+img = cv2.imread(imagePath)
+
+#convert RGB to Gray
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#Download face recognition weight from OpenCV
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+#Initialize Rectangle
+face = face_classifier.detectMultiScale(
+    gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+)
+
+#Loop Rectangle with any size to find out face on our image
+#if face is found, write rectangle on face
+for (x, y, w, h) in face:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
+
+
+#Convert Gray to RGB
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+#Display image
+import matplotlib.pyplot as plt
+plt.figure(figsize=(20,10))
+plt.imshow(img_rgb)
+plt.axis('off')
